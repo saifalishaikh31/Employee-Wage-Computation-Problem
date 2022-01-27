220cbbf2deda3affef7174ad13503bca1d972a31
saifalishaikh31 <saifalishaikh31>
2022-01-27 11:09:18 +0530

    [Saif Ali] Add: Employee Attendance Added

---
+++ EmployeeWageComputation/WageComputation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class WageComputation
+    {
+        const int EMP_PRESENT = 0, EMP_ABSENT = 1;
+        public void EmpAttendace()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck==EMP_PRESENT)
+	        {
+                Console.WriteLine("Employee is present");
+	        }
+            if (empCheck == EMP_ABSENT)
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
