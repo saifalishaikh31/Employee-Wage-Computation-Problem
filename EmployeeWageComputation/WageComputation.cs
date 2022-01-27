bcdff21b8ca9f19b1c1f0706db694f2f8d090bfb
saifalishaikh31 <saifalishaikh31>
2022-01-27 12:23:41 +0530

    [Saif Ali] Add: Daily Employee Wage Added

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -7,14 +7,16 @@ namespace EmployeeWageComputation
 {
     class WageComputation
     {
-        const int EMP_PRESENT = 0, EMP_ABSENT = 1;
-        public void EmpAttendace()
+        const int EMP_PRESENT = 0, EMP_ABSENT = 1,WAGE_PER_HR=20,FULL_DAY_HR=8;
+        public void CalculateEmpWage()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck==EMP_PRESENT)
 	        {
                 Console.WriteLine("Employee is present");
+                int dailyEmpWage = WAGE_PER_HR * FULL_DAY_HR;
+                Console.WriteLine(dailyEmpWage);
 	        }
             if (empCheck == EMP_ABSENT)
             {
